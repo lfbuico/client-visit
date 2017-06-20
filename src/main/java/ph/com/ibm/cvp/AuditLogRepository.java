060dcf986d705d7bea7b6c822a7fbfd92d415a07
Rociel Buico <buicor@ph.ibm.com>
2017-06-20 17:45:58 +0800

    set cors origin localhost

--- src/main/java/ph/com/ibm/cvp/AuditLogRepository.java
+++ src/main/java/ph/com/ibm/cvp/AuditLogRepository.java
@@ -4,8 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost", maxAge = 3600)
 @RepositoryRestResource(collectionResourceRel = "auditlog", path = "auditlog")
 public interface AuditLogRepository extends MongoRepository<AuditLog, String> {
 	
--- src/main/java/ph/com/ibm/cvp/ClientVisitApprovalRepository.java
+++ src/main/java/ph/com/ibm/cvp/ClientVisitApprovalRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-@CrossOrigin
+
+@CrossOrigin(origins = "http://localhost", maxAge = 3600)
 @RepositoryRestResource(collectionResourceRel = "cvpApproval", path = "cvpApproval")
 public interface ClientVisitApprovalRepository extends MongoRepository<ClientVisitApproval, String> {
 	
