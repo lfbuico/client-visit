453a6428ba9722f8cd90e17db90556d77467ba73
Rociel Buico <buicor@ph.ibm.com>
2017-06-20 13:05:01 +0800

    update crosconfig

--- src/main/java/ph/com/ibm/cvp/AuditLogRepository.java
+++ src/main/java/ph/com/ibm/cvp/AuditLogRepository.java
@@ -2,8 +2,10 @@ package ph.com.ibm.cvp;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
+@CrossOrigin(origins = "http://localhost", maxAge = 3600)
 @RepositoryRestResource(collectionResourceRel = "auditlog", path = "auditlog")
 public interface AuditLogRepository extends MongoRepository<AuditLog, String> {
 	
